ce0b129f214abc906f7a19bcf294521f5e86061f
岳磊 <lei.yue@aihuishou.com>
2020-12-02 15:00:15 +0800

    修复地图和视频会议链接

--- src/pages/m_navigation/index.jsx
+++ src/pages/m_navigation/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import AMapLoader from '@amap/amap-jsapi-loader';
 import { Map, Marker } from 'react-amap';
-import M_BACK from '../../assets/back.png';
+import M_BACK_ARROW  from '../../assets/back_arrow.png';
 import { withRouter } from 'react-router-dom';
 import Geolocation from 'react-amap-plugin-geolocation';
 
@@ -29,12 +29,16 @@ function Navigation (props) {
     }
     return (
         <div>
-             <img onClick={()=>window.history.back()}  style={{position:"absolute",zIndex:"100",width:"60px",bottom:"40px",right:"20px"}} src={ M_BACK }/>
-           <div style={{position:"absolute"}} className='container' id='container' style={mapStyle}>
-                    <Map center={[110.824547,19.539595]} events={amapEvents} amapkey='36812dd90b2cf9129c20bb85b10e30f7' version='1.4.15'>
+             <div style={{height:"60px",width:"100%",background:"#fff",position:"fixed"}}>
+                <img onClick={()=>window.history.back()} src={ M_BACK_ARROW } style={ {height:"20px",position:"absolute",top:"50%",marginTop:"-5px",marginLeft:"20px"} }/>
+           </div>
+          
+            <div style={ { position: "absolute",marginTop:"20px" } } className='container' id='container' style={ mapStyle }>
+                <iframe style={{border:"none"}} width={window.innerWidth} height={window.innerHeight-20} src="https://map.qq.com/m/place/info/uid=1579084311980198362&word=%E9%87%91%E7%9F%B3%E9%87%91%E9%99%B5%E9%85%92%E5%BA%97&type=point"/>
+                    {/* <Map center={[110.824547,19.539595]} events={amapEvents} amapkey='36812dd90b2cf9129c20bb85b10e30f7' version='1.4.15'>
                     <Marker position={ [ 110.824547, 19.539595 ] } events={ markerEvents } />
                       <Geolocation {...pluginProps} />
-                    </Map>
+                    </Map> */}
             </div>
          
         </div>
--- src/pages/m_pick/index.jsx
+++ src/pages/m_pick/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import M_BACK_ARROW from '../../assets/back_arrow.png';
 function Pick () {
+        const mapStyle = {
+            boxSizing: 'border-box',
+            width:  window.innerWidth,
+            height: window.innerHeight
+    }
     return (
         <div>
-            <div style={{height:"60px",width:"100%",background:"#fff",position:"relative"}}>
-                <img onClick={()=>window.history.back()} src={ M_BACK_ARROW } style={ {height:"25px",position:"absolute",top:"50%",marginTop:"-10px",marginLeft:"20px"} }/>
+            <div style={{height:"60px",width:"100%",background:"#fff",position:"fixed"}}>
+                <img onClick={()=>window.history.back()} src={ M_BACK_ARROW } style={ {height:"20px",position:"absolute",top:"50%",marginTop:"-5px",marginLeft:"20px"} }/>
            </div>
-            <div style={{marginTop:"20px"}}>
-                <iframe  style={{border:"none"}} width={ window.innerWidth } height={ window.innerHeight } src="https://meeting.tencent.com/s/zuTyJOjGvzmr" />
-            </div>
+            <div style={{position: "absolute",marginTop:"20px"}} style={ mapStyle }>
+                <iframe  style={{border:"none"}} width={ window.innerWidth } height={ window.innerHeight-20 } src="https://meeting.tencent.com/s/zuTyJOjGvzmr" />
+            </div> 
+
         </div>
     )
 };
