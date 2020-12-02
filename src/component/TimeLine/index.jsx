937c54ce52ef4c1701b2b67a32ea9f9b433267ec
岳磊 <lei.yue@aihuishou.com>
2020-12-02 15:38:27 +0800

    引入日程

--- src/component/TimeLine/index.jsx
+++ src/component/TimeLine/index.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import './index.less';
 import Y_POINT from '../../assets/y_point.png';
 import B_LINE from '../../assets/b_line.png';
-import B_POINT from '../../assets/b_point.png';
-
+import PDF_1 from '../../assets/pdf_1.png';
+import PDF_2 from '../../assets/pdf_2.png';
+import PDF_3 from '../../assets/pdf_3.png';
+import PDF_4 from '../../assets/pdf_4.png';
+import PDF_5 from '../../assets/pdf_5.png';
+import PDF_6 from '../../assets/pdf_6.png';
 function TimerLiner () {
     return (
-        <div>
-            <p className="time_date">12月3日  星期四</p>
+        <div style={{marginTop:"20px"}}>
+            <img src={ PDF_1 } style={ { width: "100%" } } />
+            <img src={ PDF_2 } style={ { width: "100%" ,marginTop:"5px"} } />
+            <img src={ PDF_3 } style={ { width: "100%" } } />
+            <img src={ PDF_4 } style={ { width: "100%" } } />
+            <img src={ PDF_5 } style={ {width:"100%"} }/>
+            <img src={ PDF_6 } style={ {width:"100%"} }/>
+            {/* <p className="time_date">12月3日  星期四</p>
             <div >
                 <div>
                     <img src={ Y_POINT } className="time_point" />
@@ -127,7 +137,7 @@ function TimerLiner () {
                 </div>
                 </div>
                
-            </div>
+            </div> */}
 
 
             
