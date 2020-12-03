0390f74ef7d63293dc029f7806c5a78b6fa55d76
岳磊 <lei.yue@aihuishou.com>
2020-12-03 15:26:09 +0800

    添加拨打电话功能

--- src/pages/m_service/index.jsx
+++ src/pages/m_service/index.jsx
@@ -18,10 +18,11 @@ function HotelService () {
            </div>
           
             <div style={ {  background:"#ffffff",  } } className='container' id='container' style={ mapStyle }>
-            <p style={ { fontSize: "24px" ,color:"#23B5FE"} }>联系我们</p> 
+                <p style={ { fontSize: "24px", color: "#23B5FE" } }>联系我们</p> 
+                
             <hr style={ {margin:"0 20px"} } color="#f5f5f5"/>
-            <p style={{fontSize:"22px"}}>岳鑫 <small style={{color:"#23B5FE",fontSize:"18px"}}>15607688773</small> </p> 
-            <p style={{fontSize:"22px"}}>范佳琪  <small style={{color:"#23B5FE",fontSize:"18px"}}>15607688773</small> </p> 
+            <p style={{fontSize:"22px"}}>岳鑫 <small  style={{fontSize:"18px"}}>156 0768 8773</small> <a style={{color:"#23B5FE",fontSize:"16px",marginLeft:"20px"}} href="tel:15607688773">立即拨打</a> </p> 
+            <p style={{fontSize:"22px"}}>范佳琪  <small  style={{fontSize:"18px"}}>135 0357 5852</small><a style={{color:"#23B5FE",fontSize:"16px",marginLeft:"20px"}} href="tel:13503575852">立即拨打</a> </p> 
            
            
                 </div>
